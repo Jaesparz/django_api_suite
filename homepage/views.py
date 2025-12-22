2b9c974b5bebbc53d819ef191f0cd541256db6ae
Jaesparz <jaesparz@espol.edu.ec>
2025-12-22 19:36:14 +0000

    avance1

---
+++ homepage/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("¡Bienvenido a la aplicación Django!")
+
+# Create your views here.
